a714a09335538a13001900b0d9f1cbe467a32822
Mike Bell <mdb036@gmail.com>
2024-02-20 19:49:16 +0000

    Use compressed LWTP/SWTP

--- gpio.h
+++ gpio.h
@@ -4,44 +4,47 @@
 
 inline static void set_outputs(uint32_t value)
 {
-    asm( "sw %[value], (tp)" : : [value] "r" (value) : "memory");
+    //asm( "sw %[value], (tp)" : : [value] "r" (value) : "memory");
+    asm( ".insn css 2, 7, %[value], 0" : : [value] "r" (value) : "memory");
 }
 
 inline static uint32_t get_outputs()
 {
     uint32_t value;
-    asm( "lw %[value], (tp)" : [value] "=r" (value) : : "memory");
+    //asm( "lw %[value], (tp)" : [value] "=r" (value) : : "memory");
+    asm( ".insn ci 2, 3, %[value], 0" : [value] "=r" (value) : : "memory");
     return value;
 }
 
 inline static uint32_t get_inputs()
 {
     uint32_t value;
-    asm volatile ( "lw %[value], 4(tp)" : [value] "=r" (value) : : "memory");
+    //asm volatile ( "lw %[value], 4(tp)" : [value] "=r" (value) : : "memory");
+    asm volatile ( ".insn ci 2, 3, %[value], 4" : [value] "=r" (value) : : "memory");
     return value;
 }
 
 inline static void set_leds(uint32_t value)
 {
-    asm( "sw %[value], 8(tp)" : : [value] "r" (value) : "memory");
+    asm( ".insn css 2, 7, %[value], 8" : : [value] "r" (value) : "memory");
 }
 
 inline static uint32_t get_leds()
 {
     uint32_t value;
-    asm( "lw %[value], 8(tp)" : [value] "=r" (value) : : "memory");
+    asm( ".insn ci 2, 3, %[value], 8" : [value] "=r" (value) : : "memory");
     return value;
 }
 
 inline static void set_midi_note(uint32_t value)
 {
-    asm( "sw %[value], 0x20(tp)" : : [value] "r" (value) : "memory");
+    asm( ".insn css 2, 7, %[value], 0xc" : : [value] "r" (value) : "memory");
 }
 
 inline static uint32_t get_midi_note()
 {
     uint32_t value;
-    asm( "lw %[value], 0x20(tp)" : [value] "=r" (value) : : "memory");
+    asm( ".insn ci 2, 3, %[value], 0xc" : [value] "=r" (value) : : "memory");
     return value;
 }
 
--- peripheral.s
+++ peripheral.s
@@ -1,25 +1,30 @@
 .section .text
 .globl uart_putc
 uart_putc:
-    lw a5, 0x14(tp)
+    #lw a5, 0x14(tp)
+    .insn ci 2, 3, a5, 0x14
     andi a5, a5, 1
     bnez a5, uart_putc
-    sw a0, 0x10(tp)
+    #sw a0, 0x10(tp)
+    .insn css 2, 7, a0, 0x10
     ret
 
 .globl uart_getc
 uart_getc:
     li a0, -1
-    lw a5, 0x14(tp)
+    #lw a5, 0x14(tp)
+    .insn ci 2, 3, a5, 0x14
     andi a5, a5, 2
     beqz a5, 1f
-    lw a0, 0x10(tp)
+    #lw a0, 0x10(tp)
+    .insn ci 2, 3, a0, 0x10
 1:
     ret
 
 .globl uart_is_char_available
 uart_is_char_available:
-    lw a0, 0x14(tp)
+    #lw a0, 0x14(tp)
+    .insn ci 2, 3, a0, 0x14
     srli a0, a0, 1
     andi a0, a0, 1
     ret
